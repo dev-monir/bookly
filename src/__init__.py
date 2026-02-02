538856237dd6020c5aa41cfd7667ee1cca48dead
Monir Sarkar <76895728+dev-monir@users.noreply.github.com>
2026-02-02 11:41:57 +0600

    Database configure successfully

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
 
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Server is Starting....")
+    await init_db()
+    yield
+    print(f"Server is Stopping....")
 
 version = "v1"
 app = FastAPI(
     title="Bookly API",
     description="Bookly API",
-    version= version
+    version= version,
+    lifespan=life_span
 )
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
 
